035c0d1b1aed7140ea5d85d045e9b0fddd720fc4
gamellekmg <146131805+gamellekmg@users.noreply.github.com>
2023-11-22 20:36:41 -0500

    add test

---
+++ Segprojet/app/src/test/java/com/example/segprojet/DeleteAccountsTest.java
@@ -0,0 +1,30 @@
+package com.example.segprojet;
+import org.junit.Test;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeleteAccountsTest {
+
+
+    @Test
+    public void testDeleteAccounts_UserDeletion() {
+
+        // Arrange
+        DeleteAccounts deleteAccounts = new DeleteAccounts();
+        deleteAccounts.listOfUsers = new ArrayList<>();
+        deleteAccounts.listOfUsers.add("userId1 email1 type1");
+        deleteAccounts.listOfUsers.add("userId2 email2 type2");
+        deleteAccounts.confirm = true;
+
+        // Act
+        deleteAccounts.onItemLongClick(/* Simuler les paramètres nécessaires */);
+
+        // Vérifier que la liste des utilisateurs est vide après la suppression
+        assertEquals(0, deleteAccounts.listOfUsers.size());
+    }
+}
